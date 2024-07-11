1f10491a8b1316cf2c4be86a220ac0335a055c56
mohammed kaif <mkaif555123@gmail.com>
2024-07-11 12:01:24 +0500

    data types write

---
+++ Day-1 Data-Types/script.js
@@ -0,0 +1,8 @@
+//Data Types
+const string = "kaif";
+const number  = 30;
+const boolean = true;
+const undefined = undefined;
+const nUll = null;
+const bigInt = 63457364863n;
+const symbol = symbol("unique")
