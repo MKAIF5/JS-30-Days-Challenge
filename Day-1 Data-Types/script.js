47c6a954244eaed79aa02ee9c36774bd1841c201
mohammed kaif <mkaif555123@gmail.com>
2024-07-12 15:57:29 +0500

    type of method

--- Day-1 Data-Types/script.js
+++ Day-1 Data-Types/script.js
@@ -4,5 +4,7 @@ const number  = 30;
 const boolean = true;
 const undefined = undefined;
 const nUll = null;
-const bigInt = 63457364863n;
+const bigInt = 6345736486n;
 const symbol = symbol("unique")
+
+console.log(typeof nUll);
