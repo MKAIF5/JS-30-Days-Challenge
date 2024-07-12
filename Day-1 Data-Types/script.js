43069e9ecfe058c12a588c43ec8d2d27c8600422
mohammed kaif <mkaif555123@gmail.com>
2024-07-12 16:09:24 +0500

    Data Types Conversion

--- Day-1 Data-Types/script.js
+++ Day-1 Data-Types/script.js
@@ -1,10 +1,24 @@
 //Data Types
-const string = "kaif";
-const number  = 30;
-const boolean = true;
-const undefined = undefined;
-const nUll = null;
-const bigInt = 6345736486n;
-const symbol = symbol("unique")
+// const string = "kaif";
+// const number  = 30;
+// const boolean = true;
+// const undefined = undefined;
+// const nUll = null;
+// const bigInt = 6345736486n;
+// const symbol = symbol("unique")
 
-console.log(typeof nUll);
+// console.log(typeof nUll);
+
+// let valueInNumber = "kaif";
+// const updatedNumber = Number(valueInNumber);
+// console.log(typeof updatedNumber);
+
+//Data Types Conversion
+// 33 => "33" =  string
+// "33" => 33 = number
+// 33 => "33abc" = NAN
+// 1 => number = true
+
+// let loggedIn = undefined;
+// const boolCheck = Boolean(loggedIn);
+// console.log(boolCheck);
