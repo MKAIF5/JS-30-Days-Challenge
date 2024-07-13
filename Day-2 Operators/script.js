cfedfbd2bea3e1b6568779a82988b7a52717ae48
mohammed kaif <mkaif555123@gmail.com>
2024-07-14 01:48:16 +0500

    operators practice before challenge

---
+++ Day-2 Operators/script.js
@@ -0,0 +1,16 @@
+// Practice Before Challenge : Operators
+
+//Arthmetic Operators
+// let a = 7;
+// let b = 3;
+// console.log("a + b = ", a + b);//Addition
+// console.log("a - b = ", a - b);//Subtraction
+// console.log("a * b = ", a * b);//Multiplication
+// console.log("a / b = ", a / b);//Division
+// console.log("a / b = ", a % b);//Modulus
+// console.log("a / b = ", a ** b);//Exponentiation
+
+// Unary Operators
+// let a = 5;
+// a++ + --a
+// console.log(a); 
