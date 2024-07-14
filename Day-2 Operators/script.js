8e671c904f6ba2d327ecb4f75c27149e9da4b42f
mohammed kaif <mkaif555123@gmail.com>
2024-07-14 23:41:50 +0500

    assigment opertor completd

--- Day-2 Operators/script.js
+++ Day-2 Operators/script.js
@@ -35,11 +35,22 @@
 
 // Activity 1 : Arthmetic Operators
 
-// Task 1 : // Write a program to add two number and log the result to the console
+// Task 1 :  Write a program to add two number and log the result to the console
 console.log(3 + 3);
 // Task 2 :  Write a program to subtract two number and log the result to the console
 console.log(5 - 3);
 // Task 3 :  Write a program to multiply two number and log the result to the console
-console.log(5 *10);
+console.log(5 * 10);
 // Task 4 :  Write a program to divide two number and log the result to the console
 console.log(10 / 5);
+
+//Activity 2 : Assigment Opertors
+
+// Task 6 : Use the += operator to add a number to a variable and log the result to the console
+let add = 1;
+add += 2
+console.log(add);
+// Task 7 : Use the -= operator to subract a number from a variable and log the result to the console
+let sub = 5;
+sub -= 4;
+console.log(sub);
