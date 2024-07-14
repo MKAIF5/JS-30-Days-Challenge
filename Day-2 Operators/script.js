85058040cb6bd3845cf0ac59b20da60877966c84
mohammed kaif <mkaif555123@gmail.com>
2024-07-15 00:03:55 +0500

    add formatting

--- Day-2 Operators/script.js
+++ Day-2 Operators/script.js
@@ -71,30 +71,30 @@ console.log(3 === 5);
 
 // Task 11 : Write a program to uses the && operator to combine two condition and
 // log the result to the console
-let userName  = "kaif";
-let locations  = "karachi";
-if(userName === "kaif" && locations === "karachi"){
+let userName = "kaif";
+let locations = "karachi";
+if (userName === "kaif" && locations === "karachi") {
     console.log("correct");
 }
-else{
+else {
     console.log("not correct");
 }
 // Task 12 :  Write a program to uses the || operator to combine two condition and
 // log the result to the console
-let age   = 15;
-let loggedIn  = true;
-if(age === 15 || loggedIn === false){
+let age = 15;
+let loggedIn = true;
+if (age === 15 || loggedIn === false) {
     console.log("correct");
 }
-else{
+else {
     console.log("not correct");
 }
 // Task 13 : Write a program to uses the != operator to combine two condition and
 // log the result to the console
-let price  = 199;
-if(price != 199){
+let price = 199;
+if (price != 199) {
     console.log("correct");
 }
-else{
+else {
     console.log("not correct");
 }
