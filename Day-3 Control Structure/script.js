87126803de7c8e88505331387fb4f9d26bf79ffe
mohammed kaif <mkaif555123@gmail.com>
2024-07-15 16:17:31 +0500

    nested if else completed

--- Day-3 Control Structure/script.js
+++ Day-3 Control Structure/script.js
@@ -5,16 +5,32 @@
 // Task 1 : Write a program to chech if a number is positive , negative  , or 
 // zero , and log the result to the result
 // let num = prompt("Enter your number");
-if(num <= -0 || num === 0){
-    console.log("negative value");
-}else{
-    console.log("positive value");
-}
+// if (num <= -0 || num === 0) {
+//     console.log("negative value");
+// } else {
+//     console.log("positive value");
+// }
 //Task 2 : Write a program to check if a person is eligible to vote (age >= 18)
 //and log the result to the console
-let voteGoverment = prompt("Enter your age");
-if(voteGoverment >= 18){
-    console.log("you have access to vote");
-}else{
-    console.log("you have not access to vote");
+// let voteGoverment = prompt("Enter your age");
+// if (voteGoverment >= 18) {
+//     console.log("you have access to vote");
+// } else {
+//     console.log("you have not access to vote");
+// }
+
+// Activity 2 : Nested If-Else-Statement
+
+// Task 3 : Write a program to find the largest three numbers using nsted if-else
+let large = prompt("enter you larger number");
+if (large > 900) {
+    if (large > 950){
+        console.log("larger number");
+    }
+    else{
+        console.log("not larger number");
+    }
+}
+else{
+    console.log("not larger number");
 }
