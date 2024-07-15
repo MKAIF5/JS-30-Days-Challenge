faafa807bcd654195d3f3c3d479126506aa7562b
mohammed kaif <mkaif555123@gmail.com>
2024-07-15 15:16:12 +0500

    if else completed

---
+++ Day-3 Control Structure/script.js
@@ -0,0 +1,20 @@
+// Challenge Started : Control Structure
+
+// Activity 1 : If-Else-Statement
+
+// Task 1 : Write a program to chech if a number is positive , negative  , or 
+// zero , and log the result to the result
+// let num = prompt("Enter your number");
+if(num <= -0 || num === 0){
+    console.log("negative value");
+}else{
+    console.log("positive value");
+}
+//Task 2 : Write a program to check if a person is eligible to vote (age >= 18)
+//and log the result to the console
+let voteGoverment = prompt("Enter your age");
+if(voteGoverment >= 18){
+    console.log("you have access to vote");
+}else{
+    console.log("you have not access to vote");
+}
