7bd43d15b0eee7af5c74c31860179651d41ab8ab
mohammed kaif <mkaif555123@gmail.com>
2024-07-17 22:15:38 +0500

    for loop task completed

---
+++ Day-4-Loops/script.js
@@ -0,0 +1,13 @@
+// Challenge Started
+
+// Activity 1 : For Loop
+
+// Task 1 : Write a program to print numbers from 1 to 10 using a for loop
+for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+}
+
+// Task 2 : Write a program to print the multiplication table of 5 using a for loop
+for (let i = 5; i <= 50; i = i + 5){
+    // console.log(i);
+}
