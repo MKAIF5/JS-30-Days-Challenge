4e294e7c70f901ab252e83eb2fc5a8ed897a76bc
mohammed kaif <mkaif555123@gmail.com>
2024-07-18 15:28:06 +0500

    function practice done

--- Day-5-Functions/script.js
+++ Day-5-Functions/script.js
@@ -1,6 +1,35 @@
 // Practice before challenge
 
-function addTwoNumbers(num1 , num2){
-   console.log(num1 + num2);
+// function addTwoNumbers(num1 , num2){
+//    console.log(num1 * num2);
+// }
+
+// function addTwoNumbers(num1, num2) {
+//     return num1 + num2
+// }
+// console.log(addTwoNumbers(5, 5));
+
+// function userLoggin(username = "sam"){
+//     if(!username){
+//       console.log("please enter a user name");
+//       return
+//     }
+//     return `${username} logged in`
+// }
+// console.log(userLoggin());
+
+// function addTwoNumbers(...cart){
+//   return cart
+// }
+// console.log(addTwoNumbers(100 , 500 , 657 , 364));
+
+const myObj =  {
+    userName: "kaif",
+    age : 15,
+    loggedIn: true
+};
+
+function getObj(anyobject){
+    console.log(`username is ${anyobject.userName} age is ${anyobject.age} and loggin is ${anyobject.loggedIn}`);
 }
-addTwoNumbers(1 , 4)
+getObj(myObj)
