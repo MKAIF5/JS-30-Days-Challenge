177e646213c357571e8116fa1b984ce619fec7ac
mohammed kaif <mkaif555123@gmail.com>
2024-07-18 15:41:28 +0500

    function declaration completed

--- Day-5-Functions/script.js
+++ Day-5-Functions/script.js
@@ -23,13 +23,36 @@
 // }
 // console.log(addTwoNumbers(100 , 500 , 657 , 364));
 
-const myObj =  {
-    userName: "kaif",
-    age : 15,
-    loggedIn: true
-};
+// const myObj =  {
+//     userName: "kaif",
+//     age : 15,
+//     loggedIn: true
+// };
 
-function getObj(anyobject){
-    console.log(`username is ${anyobject.userName} age is ${anyobject.age} and loggin is ${anyobject.loggedIn}`);
+// function getObj(anyobject){
+//     console.log(`username is ${anyobject.userName} age is ${anyobject.age} and loggin is ${anyobject.loggedIn}`);
+// }
+// getObj(myObj)
+
+
+// Challenge Started
+
+// Activity 1 : Function Declaration
+
+// Task 1 : Write a function to check if a number is odd or even and log the result
+// to the console
+function checkNumber(num) {
+    if(num % 2 === 0){
+        // console.log("even");
+    }
+    else{
+        // console.log("odd");
+    }
 }
-getObj(myObj)
+checkNumber()
+
+// Task 2 : Write a function to calculate the square of a number and return the result
+function square(x){
+    return x * x
+}
+console.log(square(5));
