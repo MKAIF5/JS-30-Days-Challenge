184ecc10fd12e9fbb74eaef6d779c094151b59b2
mohammed kaif <mkaif555123@gmail.com>
2024-07-19 00:39:26 +0500

    function expression completed

--- Day-5-Functions/script.js
+++ Day-5-Functions/script.js
@@ -42,17 +42,35 @@
 // Task 1 : Write a function to check if a number is odd or even and log the result
 // to the console
 function checkNumber(num) {
-    if(num % 2 === 0){
+    if (num % 2 === 0) {
         // console.log("even");
     }
-    else{
+    else {
         // console.log("odd");
     }
 }
 checkNumber()
 
 // Task 2 : Write a function to calculate the square of a number and return the result
-function square(x){
+function square(x) {
     return x * x
 }
 console.log(square(5));
+
+// Activity 2 : Function Expression
+
+// Task 3 : Write a function expression to find the maximum of two numbers log the 
+// result to the console
+
+function max(num1, num2) {
+    console.log(`the maximum is ${num1} ${num2} and ${Math.max(num1, num2)}`);
+}
+const num1 = 5;
+const num2 = 10;
+max(num1 , num2)
+
+// Task 4 : Write a function to concatenate two string and return the result
+function concatenate(str1 , str2){
+    return str1 + str2
+}
+console.log(concatenate("mohammad" , "kaif"));
