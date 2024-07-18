e616c2f645bf5c1a4ea8858d7cb9f63fb044aa52
mohammed kaif <mkaif555123@gmail.com>
2024-07-18 14:46:38 +0500

    loop constrol statement completd

--- Day-4-Loops/script.js
+++ Day-4-Loops/script.js
@@ -52,11 +52,30 @@ do {
 // Activity 4 : Nested Loops
 
 // Task 7 : write a program to print a pattern using a nested for loops
-
 for (let i = 1; i <= 5; i++) {
     let pattern = "";
     for (let j = 1; j <= 5; j++) {
         pattern += "*"
     }
-    console.log(pattern);
+    // console.log(pattern);
+}
+
+// Activity 5 : Loop Control Statement
+
+// Task 8 : Write a program to print numbers from 1 to 10 but skip the 
+// 5 using a continue statement
+for(let i = 1; i <= 10; i++){
+    if(i == 5){
+        continue;
+    }
+    // console.log("5 is skipped");
+}
+
+// Task 9 : Write a program to print numbers from 1 to 10 but stop the loop when 
+// the number is 7 using the break statement 
+for(let i = 1; i <= 10; i++){
+    if(i == 7){
+        break;
+    }
+    console.log("loop stoped");
 }
