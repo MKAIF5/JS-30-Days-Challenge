dd49a210e56f0809a3153bd2eaf939dd8e643d18
mohammed kaif <mkaif555123@gmail.com>
2024-07-19 19:06:56 +0500

    Array creation and access completed

---
+++ Day-6-Arrays/script.js
@@ -0,0 +1,15 @@
+// Practice Before challenge
+
+
+
+
+
+// Activity 1 : Array Creation and Access
+
+// Task 1 : Create an array of numbers from 1 to 5 and log the array to the console
+const myArr = [1, 2, 3, 4, 5];
+console.log(myArr);
+
+// Task 2 : Access the first and last elements of the array and log them to the console
+const arrAccess = ["array" , 15 , true , undefined];
+console.log(arrAccess[0] , arrAccess[3]);
