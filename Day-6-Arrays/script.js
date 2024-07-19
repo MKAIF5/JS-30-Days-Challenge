6b92ca92d72dcc3d31f52e4f4bb0a609a3f53f97
mohammed kaif <mkaif555123@gmail.com>
2024-07-19 20:53:52 +0500

    activity postponed because array practice

--- Day-6-Arrays/script.js
+++ Day-6-Arrays/script.js
@@ -1,15 +1,32 @@
 // Practice Before challenge
 
+// let arr = ["kaif" , 15 , "karachi" , false , 91 , "cricket" , false];
+// let arr1 = ["kashif" , 33 , "kfc job"];
+// let arr2 = ["safia" , 33 , "mcdonald job"];
 
+// let allArr = arr.concat(arr1);
+// let allArr2 = allArr.concat(arr2);
+// console.log(allArr2);
 
+// let allName = [...arr , ...arr1 , ...arr2];
+// console.log(allName);
+
+// let newArr = arr.join()
+
+// console.log(typeof newArr);
+// console.log(typeof arr);
+// console.log(arr);
+// console.log(newArr);
+// console.log(arr.splice(1,3));
+// console.log(arr.slice(1,3));
 
 
 // Activity 1 : Array Creation and Access
 
 // Task 1 : Create an array of numbers from 1 to 5 and log the array to the console
 const myArr = [1, 2, 3, 4, 5];
-console.log(myArr);
+// console.log(myArr);
 
 // Task 2 : Access the first and last elements of the array and log them to the console
 const arrAccess = ["array" , 15 , true , undefined];
-console.log(arrAccess[0] , arrAccess[3]);
+// console.log(arrAccess[0] , arrAccess[3]);
