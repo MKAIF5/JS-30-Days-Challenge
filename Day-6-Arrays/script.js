8f8a46c6a17372b0c13dc896423ea93ee6e218d6
mohammed kaif <mkaif555123@gmail.com>
2024-07-20 15:07:37 +0500

    array methods (intermediate)completed

--- Day-6-Arrays/script.js
+++ Day-6-Arrays/script.js
@@ -54,6 +54,55 @@
 
 // For Each
 
+// const coding = ["js", "py", "cpp", "java", "typ"];
+// coding.forEach(function (programming) {
+//     console.log(programming);
+// })
+
+// const myCoding = [
+//     {
+//         languageName: "javascript",
+//         fileName: "js"
+//     },
+//     {
+//         languageName: "python",
+//         fileName: "py"
+//     },
+//     {
+//         languageName: "typescript",
+//         fileName: "ts"
+//     },
+// ];
+
+// const values = myCoding.forEach((item) => {
+//     console.log(item.languageName)
+// })
+// console.log(values);
+
+
+// Filter
+
+// const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let filNums = nums.filter((num) => num > 4 )
+// console.log(filNums);
+
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+];
+
+const getarray = books.filter((bk) => bk.genre === "History")
+
+console.log(getarray);
+
 // Challenge Started
 
 // Activity 1 : Array Creation and Access
@@ -70,24 +119,49 @@ const arrAccess = ["array", 15, true, undefined];
 
 // Task 3 : Use the (push) method to add a new number to the end of the array and
 // log the updated array
-let pushArr = ["push" , "pop" , "unshift"];
+let pushArr = ["push", "pop", "unshift"];
 pushArr.push("shift");
 // console.log(pushArr);
 
 // Task 4 : Use the (pop) method to remove the last element from the array and log
 // the updated array
-let popArr = ["push" , "pop" , "unshift" , "shift" , "sdhufh"];
+let popArr = ["push", "pop", "unshift", "shift", "sdhufh"];
 popArr.pop();
 // console.log(popArr);
 
 // Task 5 :  Use the (shift) method to remove the first element from the array and log
 // the updated array
-let shiftArr = ["shdjfdf" , "push" , "pop" , "unshift" , "shift" ];
+let shiftArr = ["shdjfdf", "push", "pop", "unshift", "shift"];
 shiftArr.shift();
 // console.log(shiftArr);
 
 // Task 6 :  Use the (unshift) method to add a new number to the beginning of the
 // array and log the updated array
-let unshiftArr = ["pop" , "unshift" , "shift" ,];
+let unshiftArr = ["pop", "unshift", "shift",];
 unshiftArr.unshift("push");
 // console.log(unshiftArr);
+
+// Activity 3 : Array Methods (Intermidiate)
+
+// Task 7 : Use the (map) method to create a new array where each number is doubled
+// and log the new array
+let mapArray = [1, 2, 3, 4, 5];
+const mapDouble = mapArray.map((e) => {
+    return e * 2
+})
+// console.log(mapDouble);
+
+// Task 8 : Use the filter methods to create a new array with only even numbers
+// and log the new array
+let filterArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const evenFilter = filterArray.filter((even) => {
+    return even % 2 === 0
+})
+// console.log(evenFilter);
+
+// Task 9 : Use the reduce methods to calculate the sum of all numbers in the array
+// and log the result
+const reduceArray = mapArray.reduce((accumlater, currentValue) => {
+    console.log(accumlater, currentValue)
+    return accumlater + currentValue;
+}, 0)
