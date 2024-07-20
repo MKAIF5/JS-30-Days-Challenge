116d50f565ff0b528ffa3b0ad8077e302d166419
mohammed kaif <mkaif555123@gmail.com>
2024-07-20 21:17:18 +0500

    Tasks Completed

--- Day-7-Objects/script.js
+++ Day-7-Objects/script.js
@@ -9,18 +9,18 @@ const books = {
     author: "science",
     year: 2000
 }
-// console.log(books);
+console.log(books);
 
 // Task 2 : Access and log the title and author properties of the book object
-// console.log(books.title);
-// console.log(books.author);
+console.log(books.title);
+console.log(books.author);
 
 // Activity 2 : Object Methods
 
 // Task 3 : Add a method to the book object that returns a string with book's
 // title and author, and log the result of calling this method
-// console.log(books["title"]);
-// console.log(books["author"]);
+console.log(books["title"]);
+console.log(books["author"]);
 
 // Task 4 : Add a method to the book object that takes a paremeter (year) and updates 
 // the books year property , then log the updated object
@@ -29,7 +29,7 @@ books.setYear = function (updateYear) {
     this.year = updateYear;
 }
 books.setYear(2003)
-// console.log(books);
+console.log(books);
 
 // Activity 3 : Nested Object
 
@@ -72,3 +72,18 @@ books.kuchbhi = function kuchbhi() {
     return `${this.title} ${this.year}`
 }
 console.log(books.kuchbhi());
+
+// Activity 5 : Object iteration
+
+// Task 8 : Use in a for...in loop iterate over the properties of the book object
+// and log each properties and its value
+for (const key in library.books) {
+     console.log(library.books[key]);
+}
+
+// Task 9 : Use object.keys and object.values methods and log all keys and values 
+// of the book object
+console.log(Object.keys(library.books));
+console.log(Object.values(library.books));
+
+// Tasks Completed
