efaa09dec03fbf2782481e5ba2fed3130fce42a3
mohammed kaif <mkaif555123@gmail.com>
2024-07-20 16:21:26 +0500

    object methods completed

--- Day-7-Objects/script.js
+++ Day-7-Objects/script.js
@@ -14,3 +14,19 @@ console.log(books);
 // Task 2 : Access and log the title and author properties of the book object
 console.log(books.title);
 console.log(books.author);
+
+// Activity 2 : Object Methods
+
+// Task 3 : Add a method to the book object that returns a string with book's
+// title and author, and log the result of calling this method
+console.log(books["title"]);
+console.log(books["author"]);
+
+// Task 4 : Add a method to the book object that takes a paremeter (year) and updates 
+// the books year property , then log the updated object
+
+books.setYear = function (updateYear){
+    this.year = updateYear;
+}
+books.setYear(2003)
+console.log(books);
