bea8a0e13ae689f6f97b01c218df1881186d2db5
mohammed kaif <mkaif555123@gmail.com>
2024-07-20 15:49:35 +0500

    object creation and access

---
+++ Day-7-Objects/script.js
@@ -0,0 +1,16 @@
+// Challenge Started 
+
+// Activity 1 : Object Creation And Access
+
+// Task 1 : Create an object repreasenting a book with properties like title 
+// author and year, and log the object to the console
+const books = {
+    title: "history",
+    author: "science",
+    year: 2000
+}
+console.log(books);
+
+// Task 2 : Access and log the title and author properties of the book object
+console.log(books.title);
+console.log(books.author);
