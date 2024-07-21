90641f89dfd15305446ce4b595510014954ce058
mohammed kaif <mkaif555123@gmail.com>
2024-07-21 15:23:56 +0500

    template literals completed

---
+++ Day-8-ES6-Features/script.js
@@ -0,0 +1,14 @@
+// Challenge Started
+
+// Activity 1 : Template Literals
+
+// Task 1 : Use Template literals to create a string that includes variables 
+// for a person name and age, and log the string to the console
+let naMe = "kaif";
+let age = "15";
+console.log(`name is ${naMe} , age is ${age}`);
+
+// Task 2 : Create a multi-line string using templete literals and log it to the console
+let multiLine = `Create a multi-line string using templete 
+literals and log it to the console`
+console.log(multiLine);
