55cb282787d73967dad28507b78d6661619e5aec
mohammed kaif <mkaif555123@gmail.com>
2024-07-22 00:15:27 +0500

    challenge before practice

---
+++ Day-9-Dom-Manuplation/script.js
@@ -0,0 +1,30 @@
+// Practice Before Challenge
+
+// let getTag = document.getElementById("title")
+// let getId = document.getElementById("title").id
+// let getClass = document.getElementById("title").className
+// console.log(getTag);
+// console.log(getId);
+// console.log(getClass);
+
+// let parent = document.querySelector(".parent");
+// console.log(parent.children[1].innerHTML);
+
+// for (let i = 0; i < parent.children.length; i++) {
+//    console.log(parent.children[i].innerHTML);
+    
+// }
+
+// console.log(parent.firstElementChild.innerHTML);
+// console.log(parent.lastElementChild.innerHTML);
+
+// const dayMonday = document.querySelector(".day");
+// console.log(dayMonday.parentElement);
+// console.log(dayMonday.nextElementSibling.innerHTML);
+
+// console.log("NODES" , parent.childNodes);
+
+
+
+
+// Challenge Started
