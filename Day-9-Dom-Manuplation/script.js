8694c1816acb6f14d72edcbbf9d1d66f1390d97d
mohammed kaif <mkaif555123@gmail.com>
2024-07-22 00:33:45 +0500

    practice continue

--- Day-9-Dom-Manuplation/script.js
+++ Day-9-Dom-Manuplation/script.js
@@ -12,7 +12,7 @@
 
 // for (let i = 0; i < parent.children.length; i++) {
 //    console.log(parent.children[i].innerHTML);
-    
+
 // }
 
 // console.log(parent.firstElementChild.innerHTML);
@@ -24,6 +24,16 @@
 
 // console.log("NODES" , parent.childNodes);
 
+// const div = document.createElement("div");
+// div.className = "main";
+// div.id = Math.ceil(Math.random() * 10);
+// div.setAttribute("title", "title");
+// div.style.backgroundColor = "green";
+// div.style.padding = "13px"
+// // div.innerText = "my div";
+// const addTextDiv = document.createTextNode("my div");
+// div.appendChild(addTextDiv);
+// document.body.appendChild(div)
 
 
 
