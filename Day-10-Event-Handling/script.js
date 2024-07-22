f16823260392805cd96610cf8047c331ac6d688f
mohammed kaif <mkaif555123@gmail.com>
2024-07-22 21:25:30 +0500

    basic event handling completed

--- Day-10-Event-Handling/script.js
+++ Day-10-Event-Handling/script.js
@@ -4,4 +4,13 @@
 
 // Task 1 : Add a click event listener to a button that changes the text content 
 // of a paragraph
- 
+const button = document.getElementById("btn");
+button.addEventListener("click", () => {
+    button.textContent = "updated button"
+}) 
+
+// Task 2 : Add a double click event listener to an image that toggles its visiblity
+const img  = document.getElementById("img");
+img.addEventListener("dblclick" ,  () =>{
+   img.style.visibility = "hidden"
+})
