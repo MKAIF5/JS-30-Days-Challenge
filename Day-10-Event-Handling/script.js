d002cdf72e0409c3091f68f7cf51bb935291e31e
mohammed kaif <mkaif555123@gmail.com>
2024-07-23 16:06:26 +0500

    Event Delegation Completed

--- Day-10-Event-Handling/script.js
+++ Day-10-Event-Handling/script.js
@@ -74,3 +74,30 @@ const paragraph = document.getElementById("para");
 dropDown.addEventListener("change" , (e) =>{
     paragraph.textContent = `selected value : ${e.target.value}`
 });
+
+// Activity 5 : Event Delegation
+
+// Task 9 : Add a (click) event listener to a list that logs the text content of the
+// clicked list item using event delegation
+const ul = document.getElementById("list");
+ul.addEventListener("click" , (e) =>{
+    if(e.target.tagName === "LI"){
+        console.log(`checked  , ${e.target.textContent}`);
+    }
+});
+
+// Task 10 : Add an event listener to a parent element that listens for events from
+// dynamically added child elements
+const myDiv = document.getElementById("main");
+const btn = document.getElementById("btn-1");
+let count = 1;
+btn.addEventListener("click" , () =>{
+    const p = document.createElement("p");
+    p.textContent = count++
+    p.style.color = "yellow" 
+    p.style.backgroundColor = "red"
+    p.style.padding = "20px"
+    p.style.width = "30px"
+    p.style.height = "10px"
+    document.body.appendChild(p)
+})
