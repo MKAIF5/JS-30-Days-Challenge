ee6728472de005ff8be59bce675c4a834f384221
mohammed kaif <mkaif555123@gmail.com>
2024-07-27 15:13:26 +0500

    Named and Default exports completed

--- Day-13-Modules/modules.js
+++ Day-13-Modules/modules.js
@@ -1,3 +1,7 @@
-import { func , user } from "./script.js";
-console.log(func(2 , 5));
+import { func, user, add, mul, div , min } from "./script.js";
+console.log(func(2, 5));
 console.log(user.isLogged);
+console.log(add(10, 5));
+console.log(mul(10, 5));
+console.log(div(10, 5));
+console.log(min());
--- Day-13-Modules/script.js
+++ Day-13-Modules/script.js
@@ -4,17 +4,43 @@
 
 // Task 1 : Create a module that exports a function to add two numbers. import and 
 // use this module in another script
-const func = function addtwo(n1 , n2){
+const func = function addtwo(n1, n2) {
     return n1 + n2
 }
 
-export {func};
+export { func };
 
-// Task2 : Create a module that exports an object representing a person with Properties
+// Task 2 : Create a module that exports an object representing a person with Properties
 // and method. import and use this modules in anothor scripts.
 const user = {
     name: 'kaif',
     age: 15,
     isLogged: true
 };
-export {user};
+export { user };
+
+// Activity 2 : Named and Default exports
+
+// Task 3 : Create a module that exports multiple functions using named exports. import
+// and use functions in another scripts
+const add = (a, b) => {
+    return a + b
+}
+
+const mul = (m1, m2) => {
+    return m1 * m2
+}
+
+const div = (d1 , d2) => {
+   return d1 / d2
+}
+
+export {add}
+export {mul}
+export {div}
+
+// Task 4 :  Create a module that exports a single functions using default exports. 
+// import and use this function in another scripts
+export default function min(){
+    return 3 - 1
+}
