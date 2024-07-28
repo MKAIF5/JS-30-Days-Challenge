ddbe184a83ed56c7f87ff9ee6a68ef985abad025
mohammed kaif <mkaif555123@gmail.com>
2024-07-28 16:10:15 +0500

    class inheritance completed

--- Day-14-Classes/script.js
+++ Day-14-Classes/script.js
@@ -1,24 +1,46 @@
 // Practice Before Challenge
 
-class User {
-    constructor(username, email, password) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
-    }
+// class User {
+//     constructor(username, email, password) {
+//         this.username = username;
+//         this.email = email;
+//         this.password = password;
+//     }
 
-    changePassword() {
-        return `${this.password}89`
-    }
-}
+//     changePassword() {
+//         return `${this.password}89`
+//     }
+// }
 
-const getUser = new User("kaif", "kaif@test.com", "1234567");
+// const getUser = new User("kaif", "kaif@test.com", "1234567");
 
-console.log(getUser.changePassword());
+// console.log(getUser.changePassword());
 
+// inheritance class
 
+// class User {
+//     constructor(username) {
+//         this.username = username;
+//     }
+//     logMe() {
+//        console.log(`Username is ${this.username}`);
+//     }
+// }
 
+// class Teacher extends User{
+//     constructor(username , email , password){
+//         super(username)
+//         this.email = email
+//         this.password = password
+//     }
+//     addCourse(){
+//         console.log(`add new course is ${this.username}`);
+//     }
+// }
 
+// const getUsers = new Teacher("webDevelpment" , "kaif@coding.com" , "1234567");
+
+// getUsers.addCourse()
 
 // Challenge Started
 
@@ -54,5 +76,31 @@ class Persons {
         return `${this.age}15`
     }
 }
-const getPersons = new Persons("kaif" , );
-console.log(getPersons.changePassword());
+const getPersons = new Persons("kaif",);
+console.log(getPersons.changeAge());
+
+// Activity 2 : Class Inheritance
+
+// Task 3 : Define a class student that extends a person class. Add a property studentId
+// and a method to return student ID. Create an instance of the class class and log the
+// student ID.
+class Student extends Person {
+  constructor(name, age, id) {
+    super(name, age);
+    this.studentId = id;
+  }
+  getStudentId() {
+    return this.studentId;
+  }
+}
+const getId = new Student("kaif", 30, 2);
+console.log("Student ID of kaif", getId.getStudentId());
+console.log("Greet kaif", getId.greet());
+
+// Task 4 : Override the greeting method in the student class to include the student Id
+// in the message. Log the ovveriden greeting message
+Student.prototype.greet = function () {
+    return `Welcome ${this.name} Id:(${this.studentId})`;
+  };
+  console.log("Greet kaif", getId.greet());
+  console.groupEnd();   
