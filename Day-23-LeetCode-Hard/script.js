9770795dea3969b937c4f2c0e807a427e77c1574
mohammed kaif <mkaif555123@gmail.com>
2024-08-04 16:40:55 +0500

    median of two sorted array completed

--- Day-23-LeetCode-Hard/script.js
+++ Day-23-LeetCode-Hard/script.js
@@ -1,1 +1,26 @@
 // Challenge Started
+
+// Activity 1 :  Median of Two Sorted Arrays
+
+// Task 1: Solve the Median of Two Sorted Arrays problem on LeetCode.
+// Write a function that takes two sorted arrays of integers and returns the median of
+// the two sorted arrays, Log the median for a few test cases, including edge cases.
+let findMedianSortedArrays = function(nums1, nums2) {
+    // Concatenate the two arrays into a single array `arr`
+    let arr = nums1.concat(nums2);
+    // Sort the combined array in ascending order
+    arr.sort((a, b) => (a - b));
+
+    // Get the length of the combined and sorted array
+    let n = arr.length;
+    
+    // Check if the length of the array is even
+    if (n % 2 === 0) {
+        // If even, calculate the median as the average of the two middle numbers
+        return ((arr[n / 2] + arr[n / 2 - 1]) / 2);
+    } else {
+        // If odd, the median is the middle number
+        return arr[Math.floor(n / 2)];
+    }
+};
+console.log(findMedianSortedArrays([1,3], [2]));
