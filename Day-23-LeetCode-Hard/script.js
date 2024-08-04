9ed83da1944dff21c7cde8afeaccd63c9822563a
mohammed kaif <mkaif555123@gmail.com>
2024-08-04 19:12:39 +0500

    merge k sorted list

--- Day-23-LeetCode-Hard/script.js
+++ Day-23-LeetCode-Hard/script.js
@@ -24,3 +24,72 @@ let findMedianSortedArrays = function(nums1, nums2) {
     }
 };
 console.log(findMedianSortedArrays([1,3], [2]));
+
+
+// Activity 2 : Merge k Sorted Lists
+
+// Task 2 : Solve the "Merge k Sorted Lists problem on LeetCode.
+// Write a function that takes an array of k linked lists, each linked list is sorted in ascending order, and merges all the Linked lists into one sorted linked list.
+// Create a few test cases with linked lists and log the merged list.
+
+const mergeKLists = function(lists) {
+    if (lists.length === 0) {
+        return null;
+    }
+
+    // Continue merging until only one list remains
+    while (lists.length > 1) {
+        // Take the first two lists from the array
+        let list1 = lists.shift();
+        let list2 = lists.shift();
+
+        // Merge the two lists and get the result
+        let mergedList1List2 = mergeTwoLists(list1, list2);
+
+        // Add the merged list back to the array
+        lists.push(mergedList1List2);
+    }
+
+    // Return the single merged list
+    return lists[0];
+}
+
+/**
+ * Merge two sorted linked lists into one sorted linked list
+ */
+function mergeTwoLists(list1, list2) {
+    // Create a dummy node to simplify the merging process
+    let curr = new ListNode();
+    let dummy = curr;
+
+    // While both lists are not empty
+    while (list1 && list2) {
+        // Compare the values of the current nodes in both lists
+        if (list1.val < list2.val) {
+            // Attach the smaller node to the merged list
+            curr.next = list1;
+            // Move to the next node in list1
+            list1 = list1.next;
+        } else {
+            // Attach the smaller node to the merged list
+            curr.next = list2;
+            // Move to the next node in list2
+            list2 = list2.next;
+        }
+        // Move the current pointer in the merged list
+        curr = curr.next;
+    }
+
+    // If there are remaining nodes in list1, attach them
+    if (list1) {
+        curr.next = list1;
+    }
+
+    // If there are remaining nodes in list2, attach them
+    if (list2) {
+        curr.next = list2;
+    }
+
+    // Return the merged list starting from the node after the dummy
+    return dummy.next;
+};
