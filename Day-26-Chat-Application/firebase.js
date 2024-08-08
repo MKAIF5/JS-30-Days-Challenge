11ee82a5897fc192b05e3c155f97c23ba07067eb
mohammed kaif <mkaif555123@gmail.com>
2024-08-08 20:25:06 +0500

    Task Completed

---
+++ Day-26-Chat-Application/firebase.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getDatabase, ref, set, remove, onChildAdded, onChildRemoved } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAVtrxI3qVFxFG3rhcAm3PNq1O5CoZZozc",
+  authDomain: "chat-application-53142.firebaseapp.com",
+  projectId: "chat-application-53142",
+  storageBucket: "chat-application-53142.appspot.com",
+  messagingSenderId: "934311044892",
+  appId: "1:934311044892:web:09573d2bd9fe158cb7bcd9"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export {
+  db,
+  ref,
+  set,
+  remove,
+  onChildAdded,
+  onChildRemoved
+}
---
+++ Day-26-Chat-Application/script.js
@@ -0,0 +1,55 @@
+import {
+    db,
+    ref,
+    set,
+    remove,
+    onChildAdded,
+    onChildRemoved
+} from "./firebase.js";
+
+const msgText = document.querySelector("#msg-text")
+let sender;
+
+if (sessionStorage.getItem("sender")) {
+    sender = sessionStorage.getItem("sender")
+}
+
+else {
+    sender = prompt("PLEASE ENTER YOUR NAME");
+    sessionStorage.setItem("sender", sender)
+}
+
+module.sendMsg = function sendMsg() {
+    let msg = msgText.value;
+    let timeStamp = new Date().getTime()
+    set(ref(db, 'messages/' + timeStamp), {
+        msg : msg,
+        sender : sender
+    });
+}
+
+onChildAdded(ref(db, "messages"), (data) => {
+    if (data.val().sender == sender) {
+        message.innerHTML += `
+        <div style="justify-content: end; " class="outer" id="${data.key}">
+        <div id="inner" class="me">YOU : ${data.val().msg} <button id="dltMsg" onclick="module.dltMsg(${data.key})">DELETE</button> </div>
+    </div>`
+    }
+    else {
+        message.innerHTML += `
+        <div id="${data.key}" class="outer">
+        <div id="inner" class="notMe">${data.val().sender} : ${data.val().msg}</div>
+    </div>`
+    }
+});
+
+module.dltMsg = function dltMsg(key) {
+    remove(ref(db, "messages/" + key))
+}
+
+onChildRemoved(ref(db, "messages"), (data) => {
+    let msgBox = document.getElementById(data.key);
+    message.removeChild(msgBox);
+});
+
+// Task Completed
